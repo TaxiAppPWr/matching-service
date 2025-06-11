0beb7088037e9e25d5988fa2e094822383d9708e
Jakub Mikłasz <jmiklasz21@gmail.com>
2025-06-11 05:21:01 +0200

    Fix table search

--- src/main/kotlin/org/taxiapp/matching/repository/DriverRepository.kt
+++ src/main/kotlin/org/taxiapp/matching/repository/DriverRepository.kt
@@ -5,6 +5,8 @@ import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
 import aws.sdk.kotlin.services.dynamodb.model.DeleteItemRequest
 import aws.sdk.kotlin.services.dynamodb.model.GetItemRequest
 import aws.sdk.kotlin.services.dynamodb.model.PutItemRequest
+import aws.sdk.kotlin.services.dynamodb.model.QueryRequest
+import aws.sdk.kotlin.services.dynamodb.query
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
@@ -15,7 +17,8 @@ import org.taxiapp.matching.dto.dynamoDB.DriverStatusRecord
 class DriverRepository(
     private val dynamoDbClient: DynamoDbClient,
     @Value("\${aws.dynamodb.driver-status-table}") private val driverStatusTable: String,
-    @Value("\${aws.dynamodb.driver-connections-table}") private val driverConnectionsTable: String
+    @Value("\${aws.dynamodb.driver-connections-table}") private val driverConnectionsTable: String,
+    @Value("\${aws.dynamodb.driver-connections-index}") private val driverIndexName: String
 ) {
     private val logger = LoggerFactory.getLogger(DriverRepository::class.java)
 
@@ -65,14 +68,19 @@ class DriverRepository(
 
     // Read-only method for driver connections
     suspend fun getDriverConnection(driverId: String): String? {
-        val request = GetItemRequest {
+        val request = QueryRequest {
+            indexName = driverIndexName
             tableName = driverConnectionsTable
-            key = mapOf("driverId" to AttributeValue.S(driverId))
+            keyConditionExpression = "driverid = :driverId"
+            expressionAttributeValues = mapOf("driverId" to AttributeValue.S(driverId))
         }
 
-        val response = dynamoDbClient.getItem(request)
-        val item = response.item
+        val response = dynamoDbClient.query(request)
+        val items = response.items
 
-        return item?.get("connectionId")?.asS()
+        return if (items.isNullOrEmpty())
+            null
+        else
+            items[0]["connectionId"]?.asS()
     }
 }
